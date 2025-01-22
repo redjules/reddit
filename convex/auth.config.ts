b5dc205b7ef3f59ec3f2ceb6ef0ba175739fc147
Ana <anapedrabcn@gmail.com>
2025-01-22 18:22:12 +0100

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://novel-drum-73.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey="pk_test_bm92ZWwtZHJ1bS03My5jbGVyay5hY2NvdW50cy5kZXYk">
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <App />
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  </React.StrictMode>
+);
